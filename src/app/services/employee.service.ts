3363ba295d7b8308611eb521a6f17dac42333c36
Magda-GitHub <antoniakmagdalena@wp.pl>
2022-11-17 22:46:00 +0100

    feat: third task POST

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {PersonModel} from "../model/person.model";
+import {CreateEmployeeModel} from "../model/create-employee.model";
 
 
 @Injectable()
@@ -11,6 +12,13 @@ export class EmployeeService {
 
   getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>( 'assets/data/people.json') ;
+  }
+
+  create(employee: CreateEmployeeModel): Observable<void> {
+    return this._httpClient.post('https://dummy.restapiexample.com/create', employee).pipe(map(_ => void 0));
+
+
+
   }
 }
 
